06eb31deaab106e0308dce959f2fb48fa7087e43
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-10 15:31:21 +0530

    category section added

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,12 +1,68 @@
-import React from 'react'
-import { View,Text } from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 
 const Home = () => {
+  const [state, setState] = useState([
+    { cat: "girls" },
+    { cat: "couples" },
+    { cat: "nature" },
+    { cat: "animals" },
+    { cat: "birds" },
+    { cat: "space" },
+    { cat: "cars" },
+  ]);
+  const handlePress = (text) => {};
   return (
-    <View>
-        
-    </View>
-  )
-}
+    <View style={{ flex: 1 }}>
+      {/*Category section start*/}
+      <View style={styles.horizontal}>
+        <ScrollView horizontal={true}>
+          {state.map((ele, idx) => {
+            return (
+              <TouchableOpacity
+                key={idx + 1}
+                style={styles.category}
+                onPress={() => handlePress(ele.cat)}
+              >
+                <Text>{ele.cat}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+      {/*Category section end*/}
 
-export default Home
+      {/*Wallpaper Section Start*/}
+      <View style={{flex:1,backgroundColor:"pink"}}>
+
+      </View>
+      {/*Wallpaper Section END*/}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  horizontal: {
+    width: "100%",
+    maxHeight: 52,
+    height: "100%",
+  },
+  category: {
+    backgroundColor: "white",
+    margin: 8,
+    paddingBottom: 6,
+    paddingTop: 6,
+    paddingLeft: 12,
+    paddingRight: 12,
+    borderRadius: 16,
+    textTransform: "capitalize",
+  },
+});
+
+export default Home;
