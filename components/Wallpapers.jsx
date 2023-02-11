bc632893326177d6f91868c63a277e8b82bd2929
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-11 14:15:47 +0530

    maping from context api

--- components/Wallpapers.jsx
+++ components/Wallpapers.jsx
@@ -1,25 +1,25 @@
 import { useNavigation } from "@react-navigation/native";
-import { useEffect, useState } from "react";
-import { View, Text, ScrollView, Image,TouchableOpacity, Alert } from "react-native";
-import { api } from "../private";
+import { useContext } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import wallContext from "../context/wallpaper.context";
 const Wallpapers = () => {
-  const [state, setState] = useState([]);
-    const navigation = useNavigation()
-  const fetchWallpaper = async () => {
-    let responce = await fetch(api);
-    let wall = await responce.json();
-    setState(wall);
+  const { wall } = useContext(wallContext);
+  const navigation = useNavigation();
+
+  const handleTouch = (url) => {
+    navigation.navigate("image", { url: url });
   };
-  const handleTouch = (url)=>{
-    navigation.navigate('image',{url:url})
-  }
-  useEffect(() => {
-    fetchWallpaper();
-  }, []);
+
   return (
     <ScrollView>
       <View style={{ flex: 1 }}>
-        {state.length > 0 ? (
+        {wall.length > 0 ? (
           <View
             style={{
               flex: 1,
@@ -28,7 +28,7 @@ const Wallpapers = () => {
               justifyContent: "space-between",
             }}
           >
-            {state.map((ele) => {
+            {wall.map((ele) => {
               return (
                 <View
                   style={{
@@ -43,12 +43,14 @@ const Wallpapers = () => {
                   }}
                   key={ele.id}
                 >
-                <TouchableOpacity onPress={()=>handleTouch(ele.urls.regular)}>
-                  <Image
-                    style={{ width: "100%", height: "100%", borderRadius: 8 }}
-                    source={{ uri: ele.urls.regular }}
-                    alt={"image"}
-                  />
+                  <TouchableOpacity
+                    onPress={() => handleTouch(ele.urls.regular)}
+                  >
+                    <Image
+                      style={{ width: "100%", height: "100%", borderRadius: 8 }}
+                      source={{ uri: ele.urls.regular }}
+                      alt={"image"}
+                    />
                   </TouchableOpacity>
                 </View>
               );
