f6d7089179094d7b0a8a24f5d13fd4235f4ce09d
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-11 14:06:25 +0530

    single wall screen added

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './components/Home';
+import SingleWall from './components/SingleWall';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='home' component={Home} options={{title:"Online Wallpapers"}}/>
+        <Stack.Screen name='image' component={SingleWall} options={{title:"Single Wall"}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
